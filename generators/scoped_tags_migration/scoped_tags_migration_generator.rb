cd0d2240876462d6aca92ab1c6c3f5a386e68fd3
Danny Hiemstra <dannyhiemstra@gmail.com>
2009-10-19 15:07:34 +0200

    Fixed the migration generator (correct naming)

--- generators/scoped_tags_migration/scoped_tags_migration_generator.rb
+++ generators/scoped_tags_migration/scoped_tags_migration_generator.rb
@@ -1,4 +1,4 @@
-class ScopedTagsGenerator < Rails::Generator::Base 
+class ScopedTagsMigrationGenerator < Rails::Generator::Base 
   def manifest 
     record do |m| 
       m.migration_template 'migration.rb', 'db/migrate', :migration_file_name => "scoped_tags_migration"
